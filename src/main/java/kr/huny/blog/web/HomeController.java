e7266e92d60d3843941a74e3692fa43a4f9ef999
sousic <sousics@naver.com>
2017-06-28 15:24:23 +0900

    inits commit..

---
+++ src/main/java/kr/huny/blog/web/HomeController.java
@@ -0,0 +1,17 @@
+package kr.huny.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by sousic on 2017-06-28.
+ */
+@Controller
+public class HomeController
+{
+    @RequestMapping(value="/")
+    public String home()
+    {
+        return "home/index";
+    }
+}
